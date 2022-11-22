882e79df434e86687af78891621c6b4e62be40f2
wook@desktop05 <rudollee@gmail.com>
2022-11-22 19:44:08 +0900

    Add project files.

---
+++ WpfMvvmToolKitInit/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfMvvmToolKitInit
+{
+    [ObservableObject]
+    public partial class MainWindowViewModel
+    {
+        public MainWindowViewModel() { }
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(IncreaseCommand))]
+        private int count = 0;
+
+        [RelayCommand(IncludeCancelCommand = true)]
+        private async Task Increase(CancellationToken token)
+        {
+            try
+            {
+                await Task.Delay(1000, token);
+                Count++;
+            }
+            catch (OperationCanceledException)
+            {
+                Count--;
+            }
+        }
+
+    }
+}
